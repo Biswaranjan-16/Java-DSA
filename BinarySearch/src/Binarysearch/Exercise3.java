6df0a49acbcc48a5d74ce6e89e0e9b9e3295cc1f
=Biswaranjan <biswaranjanb627@gmail.com>
2024-11-01 18:47:39 +0530

    01/11/2024

---
+++ BinarySearch/src/Binarysearch/Exercise3.java
@@ -0,0 +1,44 @@
+package Binarysearch;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		int[] arr = { 99,88,88,77,66,55,44,33,22,11 };
+		int target = 22;
+		int ans = binarysearch(arr, target);
+		System.out.println(ans);
+
+	}
+
+	static int binarysearch(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length - 1;
+		
+		boolean isAsc=arr[start] < arr[end];
+		
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			
+			if(arr[mid] == target) {
+				return mid;
+			}
+			
+			if(isAsc) {
+				if(target < arr[mid]) {
+					end=mid-1;
+				}else {
+					start=mid+1;
+				}
+			}else {
+				if(target > arr[mid]) {
+					end=mid-1;
+				}else {
+					start=mid+1;
+				}
+			}
+		}
+		
+		return -1;
+	}
+
+}
