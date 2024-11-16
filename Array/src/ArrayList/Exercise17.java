fc0ee71a9bd972bb71e7ccfe8307b90688dd9a13
=Biswaranjan <biswaranjanb627@gmail.com>
2024-11-16 22:38:00 +0530

    16/11/2024

---
+++ Array/src/ArrayList/Exercise17.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Exercise17 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		list.add(12);
+		list.add(54);
+		list.add(32);
+		list.add(54);
+		list.add(76);
+		list.add(33);
+		list.add(99);
+
+		System.out.println(list);
+		
+		Collections.reverse(list);
+		
+		System.out.println(list);
+
+	}
+
+}
