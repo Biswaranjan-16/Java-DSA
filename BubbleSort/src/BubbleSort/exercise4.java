1306163212b51c790893e85fa6e96c7ed0dd1f7d
=Biswaranjan <biswaranjanb627@gmail.com>
2024-11-16 10:13:08 +0530

    16/11/2024

---
+++ BubbleSort/src/BubbleSort/exercise4.java
@@ -0,0 +1,34 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class exercise4 {
+
+	public static void main(String[] args) {
+		int[] arr= {54,90,65,23,1,4,34,98};
+		sort(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	 static void sort(int[] arr) {
+		boolean swapped;
+		for(int i=0;i<arr.length;i++) {
+			swapped =false;
+			for(int j=1;j<arr.length-1;j++) {
+				if(arr[j] < arr[j-1]) {
+					int temp=arr[j];
+					arr[j] =arr[j-1];
+					arr[j-1]=temp;
+					swapped=true;
+				}
+			}
+			if(!swapped) {
+				break;
+			}
+			
+		}
+		
+	}
+
+}
